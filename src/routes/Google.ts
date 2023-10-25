cd8d311fffec6c522001126c7145f46950f0c585
Devin W. Leaman <git@4lch4.email>
2023-10-24 21:47:43 -0500

    refactor: updated request body schema

--- src/routes/Google.ts
+++ src/routes/Google.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 import { Elysia, t } from 'elysia'
 import { DiscordWebhookPayload } from '../lib'
 
-const UNKNOWN_STRING = 'unknown'
-
 export const GoogleRoutes = new Elysia({ name: 'Google-Routes' })
   .onError(ctx => {
     logger.error(`[GoogleRoutes#onError]: ctx.error`, ctx.error)
@@ -13,56 +11,12 @@ export const GoogleRoutes = new Elysia({ name: 'Google-Routes' })
     logger.error(`[GoogleRoutes#onError]: ctx.params`, ctx.params)
     logger.error(`[GoogleRoutes#onError]: ctx.headers`, ctx.headers)
   })
-  .get(
-    '/google',
-    async ctx => {
-      const webhookPayload: DiscordWebhookPayload = { content: ctx.body.incident.summary }
-      const webhookRes = await axios.post(process.env.DISCORD_WEBHOOK_URL!, webhookPayload)
+  .get('/google', async ctx => {
+    // const webhookPayload: DiscordWebhookPayload = { content: ctx.body.incident.summary }
+    // const webhookRes = await axios.post(process.env.DISCORD_WEBHOOK_URL!, webhookPayload)
 
-      logger.log('success', `[GoogleRoutes#GET]: Request sent to Discord Webhook...`, {
-        body: ctx.body,
-        webhookPayload,
-        webhookRes,
-      })
-    },
-    {
-      body: t.Object({
-        version: t.String({ default: UNKNOWN_STRING }),
-        incident: t.Object({
-          incident_id: t.String({ default: UNKNOWN_STRING }),
-          scoping_project_id: t.String({ default: UNKNOWN_STRING }),
-          scoping_project_number: t.Number({ default: 0 }),
-          url: t.String({ default: UNKNOWN_STRING }),
-          started_at: t.String({ default: UNKNOWN_STRING }),
-          ended_at: t.String({ default: UNKNOWN_STRING }),
-          state: t.String({ default: UNKNOWN_STRING }),
-          summary: t.String({ default: UNKNOWN_STRING }),
-          apigee_url: t.String({ default: UNKNOWN_STRING }),
-          observed_value: t.String({ default: UNKNOWN_STRING }),
-          resource: t.Object({
-            type: t.String({ default: UNKNOWN_STRING }),
-            labels: t.Any(),
-          }),
-          resource_type_display_name: t.String({ default: UNKNOWN_STRING }),
-          resource_id: t.String({ default: UNKNOWN_STRING }),
-          resource_display_name: t.String({ default: UNKNOWN_STRING }),
-          resource_name: t.String({ default: UNKNOWN_STRING }),
-          metric: t.Object({
-            type: t.String({ default: UNKNOWN_STRING }),
-            displayName: t.String({ default: UNKNOWN_STRING }),
-            labels: t.Any(),
-          }),
-          metadata: t.Any(),
-          policy_name: t.String({ default: UNKNOWN_STRING }),
-          policy_user_labels: t.Any(),
-          documentation: t.String({ default: UNKNOWN_STRING }),
-          condition: t.Any(),
-          condition_name: t.String({ default: UNKNOWN_STRING }),
-          threshold_value: t.String({ default: UNKNOWN_STRING }),
-        }),
-      }),
-    }
-  )
+    logger.log('success', `[GoogleRoutes#GET]: Request received...`, { ctx })
+  })
   .post(
     '/google',
     async ctx => {
@@ -77,38 +31,63 @@ export const GoogleRoutes = new Elysia({ name: 'Google-Routes' })
     },
     {
       body: t.Object({
-        version: t.String({ default: UNKNOWN_STRING }),
+        version: t.String(),
         incident: t.Object({
-          incident_id: t.String({ default: UNKNOWN_STRING }),
-          scoping_project_id: t.String({ default: UNKNOWN_STRING }),
-          scoping_project_number: t.Number({ default: 0 }),
-          url: t.String({ default: UNKNOWN_STRING }),
-          started_at: t.String({ default: UNKNOWN_STRING }),
-          ended_at: t.String({ default: UNKNOWN_STRING }),
-          state: t.String({ default: UNKNOWN_STRING }),
-          summary: t.String({ default: UNKNOWN_STRING }),
-          apigee_url: t.String({ default: UNKNOWN_STRING }),
-          observed_value: t.String({ default: UNKNOWN_STRING }),
+          incident_id: t.String(),
+          scoping_project_id: t.String(),
+          scoping_project_number: t.Number(),
+          url: t.String(),
+          started_at: t.String(),
+          ended_at: t.String(),
+          state: t.String(),
+          summary: t.String(),
+          apigee_url: t.String(),
+          observed_value: t.String(),
           resource: t.Object({
-            type: t.String({ default: UNKNOWN_STRING }),
-            labels: t.Any(),
+            type: t.String(),
+            labels: t.Object({
+              example: t.String(),
+            }),
           }),
-          resource_type_display_name: t.String({ default: UNKNOWN_STRING }),
-          resource_id: t.String({ default: UNKNOWN_STRING }),
-          resource_display_name: t.String({ default: UNKNOWN_STRING }),
-          resource_name: t.String({ default: UNKNOWN_STRING }),
+          resource_type_display_name: t.String(),
+          resource_id: t.String(),
+          resource_display_name: t.String(),
+          resource_name: t.String(),
           metric: t.Object({
-            type: t.String({ default: UNKNOWN_STRING }),
-            displayName: t.String({ default: UNKNOWN_STRING }),
-            labels: t.Any(),
+            type: t.String(),
+            displayName: t.String(),
+            labels: t.Object({
+              example: t.String(),
+            }),
           }),
-          metadata: t.Any(),
-          policy_name: t.String({ default: UNKNOWN_STRING }),
-          policy_user_labels: t.Any(),
-          documentation: t.String({ default: UNKNOWN_STRING }),
-          condition: t.Any(),
-          condition_name: t.String({ default: UNKNOWN_STRING }),
-          threshold_value: t.String({ default: UNKNOWN_STRING }),
+          metadata: t.Object({
+            system_labels: t.Object({
+              example: t.String(),
+            }),
+            user_labels: t.Object({
+              example: t.String(),
+            }),
+          }),
+          policy_name: t.String(),
+          policy_user_labels: t.Object({
+            example: t.String(),
+          }),
+          documentation: t.String(),
+          condition: t.Object({
+            name: t.String(),
+            displayName: t.String(),
+            conditionThreshold: t.Object({
+              filter: t.String(),
+              comparison: t.String(),
+              thresholdValue: t.Number(),
+              duration: t.String(),
+              trigger: t.Object({
+                count: t.Number(),
+              }),
+            }),
+          }),
+          condition_name: t.String(),
+          threshold_value: t.String(),
         }),
       }),
     }
